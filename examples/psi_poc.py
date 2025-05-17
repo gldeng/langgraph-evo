8a0280412b71b652c811bf98769bbe8fa334b4c7
gldeng <realgldeng@gmail.com>
2025-05-17 22:09:14 +0800

    Add psi poc

---
+++ examples/psi_poc.py
@@ -0,0 +1,189 @@
+from typing import Annotated
+from langchain_core.tools import tool, InjectedToolCallId
+from langgraph.prebuilt import InjectedState
+from langgraph.graph import StateGraph, START, MessagesState
+from langgraph.types import Command
+from langgraph.prebuilt import create_react_agent
+
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv()
+
+from langchain_tavily import TavilySearch
+
+from langgraph.prebuilt import create_react_agent
+
+
+from langchain_core.messages import convert_to_messages
+
+
+def pretty_print_message(message, indent=False):
+    pretty_message = message.pretty_repr(html=True)
+    if not indent:
+        print(pretty_message)
+        return
+
+    indented = "\n".join("\t" + c for c in pretty_message.split("\n"))
+    print(indented)
+
+
+def pretty_print_messages(update, last_message=False):
+    is_subgraph = False
+    if isinstance(update, tuple):
+        ns, update = update
+        # skip parent graph updates in the printouts
+        if len(ns) == 0:
+            return
+
+        graph_id = ns[-1].split(":")[0]
+        print(f"Update from subgraph {graph_id}:")
+        print("\n")
+        is_subgraph = True
+
+    for node_name, node_update in update.items():
+        update_label = f"Update from node {node_name}:"
+        if is_subgraph:
+            update_label = "\t" + update_label
+
+        print(update_label)
+        print("\n")
+
+        messages = convert_to_messages(node_update["messages"])
+        if last_message:
+            messages = messages[-1:]
+
+        for m in messages:
+            pretty_print_message(m, indent=is_subgraph)
+        print("\n")
+
+
+web_search = TavilySearch(max_results=3)
+
+
+def create_handoff_tool(*, agent_name: str, description: str | None = None):
+    name = f"transfer_to_{agent_name}"
+    description = description or f"Ask {agent_name} for help."
+
+    @tool(name, description=description)
+    def handoff_tool(
+        state: Annotated[MessagesState, InjectedState],
+        tool_call_id: Annotated[str, InjectedToolCallId],
+    ) -> Command:
+        tool_message = {
+            "role": "tool",
+            "content": f"Successfully transferred to {agent_name}",
+            "name": name,
+            "tool_call_id": tool_call_id,
+        }
+        return Command(
+            goto=agent_name,  
+            update={**state, "messages": state["messages"] + [tool_message]},  
+            graph=Command.PARENT,  
+        )
+
+    return handoff_tool
+
+
+# Handoffs
+assign_to_research_agent = create_handoff_tool(
+    agent_name="research_agent",
+    description="Assign task to a researcher agent.",
+)
+
+assign_to_math_agent = create_handoff_tool(
+    agent_name="math_agent",
+    description="Assign task to a math agent.",
+)
+
+supervisor_agent = create_react_agent(
+    model="openai:gpt-4.1",
+    tools=[assign_to_research_agent, assign_to_math_agent],
+    prompt=(
+        "You are a supervisor managing two agents:\n"
+        "- a research agent. Assign research-related tasks to this agent\n"
+        "- a math agent. Assign math-related tasks to this agent\n"
+        "Assign work to one agent at a time, do not call agents in parallel.\n"
+        "Do not do any work yourself."
+    ),
+    name="supervisor",
+)
+
+from langgraph.graph import END
+
+research_agent = create_react_agent(
+    model="openai:gpt-3.5-turbo",
+    tools=[web_search],
+    prompt=(
+        "You are a research agent.\n\n"
+        "INSTRUCTIONS:\n"
+        "- Assist ONLY with research-related tasks, DO NOT do any math\n"
+        "- After you're done with your tasks, respond to the supervisor directly\n"
+        "- Respond ONLY with the results of your work, do NOT include ANY other text."
+    ),
+    name="research_agent",
+)
+# for chunk in research_agent.stream(
+#     {"messages": [{"role": "user", "content": "who is the mayor of NYC?"}]}
+# ):
+#     pretty_print_messages(chunk)
+
+def add(a: float, b: float):
+    """Add two numbers."""
+    return a + b
+
+
+def multiply(a: float, b: float):
+    """Multiply two numbers."""
+    return a * b
+
+
+def divide(a: float, b: float):
+    """Divide two numbers."""
+    return a / b
+
+
+math_agent = create_react_agent(
+    model="openai:gpt-4.1",
+    tools=[add, multiply, divide],
+    prompt=(
+        "You are a math agent.\n\n"
+        "INSTRUCTIONS:\n"
+        "- Assist ONLY with math-related tasks\n"
+        "- After you're done with your tasks, respond to the supervisor directly\n"
+        "- Respond ONLY with the results of your work, do NOT include ANY other text."
+    ),
+    name="math_agent",
+)
+
+# Define the multi-agent supervisor graph
+supervisor = (
+    StateGraph(MessagesState)
+    # NOTE: `destinations` is only needed for visualization and doesn't affect runtime behavior
+    .add_node(supervisor_agent, destinations=("research_agent", "math_agent", END))
+    .add_node(research_agent)
+    .add_node(math_agent)
+    .add_edge(START, "supervisor")
+    # always return back to the supervisor
+    .add_edge("research_agent", "supervisor")
+    .add_edge("math_agent", "supervisor")
+    .compile()
+)
+
+for chunk in supervisor.stream(
+    {
+        "messages": [
+            {
+                "role": "user",
+                "content": "find US and New York state GDP in 2024. what % of US GDP was New York state?",
+            }
+        ]
+    },
+):
+    pretty_print_messages(chunk, last_message=True)
+
+final_message_history = chunk["supervisor"]["messages"]
+
+for message in final_message_history:
+    message.pretty_print()
